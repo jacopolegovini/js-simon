4ebfd2a437f0da4ffc927bf6d85b416f28b91326
jacopolegovini <legovini96@gmail.com>
2024-08-27 17:04:59 +0200

    issue with userinput check fixed

--- script.js
+++ script.js
@@ -14,14 +14,18 @@ const secondElement = document.querySelector('.second');
 const randomNumbersElement = document.querySelector('.random-numbers');
 const randomNumberElement = document.querySelectorAll('.random-number');
 const form = document.querySelector('form');
-const numberInput = document.querySelectorAll('.number');
+const userInputs = document.querySelectorAll('.user-number');
+const buttonElement = document.querySelector('button');
+const userInputsId = document.getElementById('user-number');
+
 
 
 // Crea le variabili necessarie
 const difficulty = 99;
-let second = 5;
+let second = 1;
 let randomNumbers = []
-let userNumber = [4, 2, 5, 7, 6];
+let userInput = 0;
+
 
 // Crea le funzioni necessarie
 // Crea la funzione per trovare tot numeri random
@@ -38,10 +42,13 @@ const generateRandomNumber = difficulty => {
 }
 
 // Crea la funzione per verificare che i numeri inseriti dall'utente siano gli stessi di quelli prodotti randomicamente
-const checkNumber = (randomNumbers, userNumber) => {
+// ! Come mai non funziona?
+const checkNumber = (randomNumbers, userInputs) => {
     console.log(randomNumbers)
+    
     for (let i = 0; i < 5; i++) {
-        if (randomNumbers.includes(userNumber[i])) {
+
+        if (randomNumbers.includes(userInputs[i])) {
         console.log('true');
       } else {
         console.log('false');
@@ -49,30 +56,69 @@ const checkNumber = (randomNumbers, userNumber) => {
     }
 }
 
-generateRandomNumber(difficulty)
-checkNumber(randomNumbers, userNumber)
 
 
 
 // Fai partire il timer di tot secondi
 const interval = setInterval(() => {
+
+    // Fai partire il timer
+    // Quando scade...
     if (second === 0) {
+
+        // Blocca il timer
         clearInterval(interval);
+
+        // Crea i numeri random
+        // generateRandomNumber(difficulty);
+        generateRandomNumber(difficulty)
+        // randomNumbers = [1, 5, 9, 8, 3]
+
+        // Aggiungi e rimuovi le classi necessarie
         randomNumbersElement.classList.add('d-none');
         secondElement.classList.add('d-none')
         form.classList.remove('d-none')
+
+        // Controlla se l'input dello user è lo stesso del numero random
+        // buttonElement.addEventListener('click', function(){
+        //     for (let i = 0; i < 5; i++) {
+        //         if (randomNumbers.includes(userInput[i])) {
+        //         console.log('true');
+        //         } else {
+        //             console.log('false');
+        //         }
+        //     }
+        // })
+
+    } else {
+        console.log(second--)
+        secondElement.innerHTML = second;
     }
-    console.log(second--)
-    secondElement.innerHTML = second;
 }, 1000)
 
+// Crea l'evento sul bottone una volta che lo user ha compilato i campi
+buttonElement.addEventListener('click', function(event){
+    event.preventDefault()
+
+    // Controlla se l'input inserito è uguale al randomnumber
+    for (let i = 0; i < userInputs.length; i++) {
+        userInput = parseInt(userInputs[i].value)
+
+        if (randomNumbers.includes(userInput)) {
+            console.log('true');
+          } else {
+            console.log('false');
+          }
+    }
+})
 
 
-// const test = [1, 5, 9, 8, 3];
-// const test2 = [4, 2, 5, 7, 6];
+   
+// for (let i = 0; i < userInputs.length; i++) {
+//     buttonElement.addEventListener('form', (event) => {
+//         event.preventDefault();
 
-// for (let i = 0; i < 5; i++) {
-//     test.includes(5)
-//     console.log(test[i])
-  
+//         userInput = userInputs[i].value;
+//         checkNumber (randomNumbers, userInput)
+//     })
 // }
